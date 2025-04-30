857eedc2cebacc564f468798a8a236d1ee18521d
Bimai6 <bimaivfx@gmail.com>
2025-04-30 21:28:41 +0200

    feat: add admin routes with login redirect, add templates for content, admin, and registration, add wiew connectors

--- backend/argiobackend/argiobackend/urls.py
+++ backend/argiobackend/argiobackend/urls.py
@@ -14,12 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from argioapp import views
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('admin-login/', views.login),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('dashboard/admins/', views.admin_list, name='admin_list'),
+    path('dashboard/admins/create/', views.admin_create, name='admin_create'),
+    path('dashboard/admins/update/<int:pk>/', views.admin_update, name='admin_update'),
+    path('dashboard/admins/delete/<int:pk>/', views.admin_delete, name='admin_delete'),
+
+    path('dashboard/contents/', views.content_list, name='content_list'),
+    path('dashboard/contents/create/', views.content_create, name='content_create'),
+    path('dashboard/contents/update/<int:pk>/', views.content_update, name='content_update'),
+    path('dashboard/contents/delete/<int:pk>/', views.content_delete, name='content_delete'),
 ]
