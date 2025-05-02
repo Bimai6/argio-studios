1156872e7f36d6848213693e3ddeb7c99669e946
Bimai6 <bimaivfx@gmail.com>
2025-05-02 19:19:37 +0200

    refactor: fix settings, models, routes, migrations, to fit django admin default tables and views

--- backend/argiobackend/argioapp/admin.py
+++ backend/argiobackend/argioapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Content
 
-# Register your models here.
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_at', 'is_active')
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.groups.filter(name='SuperAdmin').exists()
