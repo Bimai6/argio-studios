cedd0ce66915a40c1199c4305373d9f41a50c8a2
Bimai6 <bimaivfx@gmail.com>
2025-05-10 19:30:47 +0200

    feat: add ListContent component with api fetch, add corsheaders to backend to unblock CORS

--- frontend/src/pages/Catalogue/Catalogue.jsx
+++ frontend/src/pages/Catalogue/Catalogue.jsx
@@ -1,6 +1,12 @@
+import ListContent from "../../components/ListContent/ListContent"
+
 const Catalogue = () => {
+
     return(
+        <>
         <h1>This is Catalogue Page</h1>
+        <ListContent />
+        </>
     )
 }
 
