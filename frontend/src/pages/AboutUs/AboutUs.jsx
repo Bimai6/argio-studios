f80824101777bd6d6b67d087e09d86449676d35a
Bimai6 <bimaivfx@gmail.com>
2025-05-10 17:54:11 +0200

    feat: add react router dom, router and pages component, and router provider in main.jsx to make the basic front end structure

---
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,7 @@
+const AboutUs = () => {
+    return(
+        <h1>This is AboutUs Page</h1>
+    )
+}
+
+export default AboutUs
---
+++ frontend/src/pages/Catalogue/Catalogue.jsx
@@ -0,0 +1,7 @@
+const Catalogue = () => {
+    return(
+        <h1>This is Catalogue Page</h1>
+    )
+}
+
+export default Catalogue
