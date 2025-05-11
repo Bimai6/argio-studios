39e70189ecfb013d06618b807ae23bf98cc56495
Bimai6 <bimaivfx@gmail.com>
2025-05-11 22:39:45 +0200

    style: fix responsiveness with grid layout for ListContent

--- frontend/src/components/ListContent/ListContent.jsx
+++ frontend/src/components/ListContent/ListContent.jsx
@@ -1,23 +1,27 @@
 import { useState, useEffect } from "react";
 
 const ListContent = () => {
-    const [contents, setContents] = useState([]);
+  const [contents, setContents] = useState([]);
 
-useEffect(() => {
-  fetch("http://localhost:8000/api/contents/")
-    .then((res) => res.json())
-    .then((data) => setContents(data));
-}, []);
+  useEffect(() => {
+    fetch("http://localhost:8000/api/contents/")
+      .then((res) => res.json())
+      .then((data) => setContents(data));
+  }, []);
 
-return (
-  <div className="grid grid-cols-3 gap-4">
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       {contents.map((content) => (
-        <div key={content.id} className="p-4 w-1/3">
-            <img src={content.url} alt={content.title} className="max-w-80 h-auto rounded-lg"/>
+        <div key={content.id} className="p-4 w-full">
+          <img
+            src={content.url}
+            alt={content.title}
+            className="max-w-80 w-full h-auto rounded-lg object-contain"
+          />
         </div>
       ))}
-  </div>
-);
-}
+    </div>
+  );
+};
 
-export default ListContent
+export default ListContent;
