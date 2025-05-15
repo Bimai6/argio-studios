aa7f98485a0e2972433b504575c423c484b3e12d
Bimai6 <bimaivfx@gmail.com>
2025-05-15 13:43:19 +0200

    feat: add filter and pagination to API in views.py

--- backend/argiobackend/argioapp/views.py
+++ backend/argiobackend/argioapp/views.py
@@ -1,7 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .serializers import ContentSerializer
 from .models import Content
+from rest_framework.pagination import PageNumberPagination
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class ContentViewSet(viewsets.ModelViewSet):
     queryset = Content.objects.all().order_by('id')
     serializer_class = ContentSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['id', 'created_at']
+    ordering = ['id']
+    pagination_class = StandardResultsSetPagination
