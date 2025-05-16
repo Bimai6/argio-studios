8d083ebe165dcbf7f24979453a5bd556719f9439
Bimai6 <bimaivfx@gmail.com>
2025-05-16 19:02:02 +0200

    refactor: change to Home, make their structure clearer, add some styles

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,21 +1,23 @@
 import Nav from "../../components/Nav/Nav"
 import { Link } from "react-router"
+import filterItems from "../../utils/Arrays"
 
 const Home = () => {
-
-    return(
+    return (
         <>
-            <div>
-                <Link to='/catalogue?content_type=0'>vídeos</Link>
+            <div className="bg-black h-screen text-white relative">
+                <div className="grid grid-cols-3 grid-rows-3 h-full place-items-center text-xl sm:text-2xl md:text-4xl xl:text-5xl">
+                    {filterItems.map((item, index)=> (
+                        <div key={index} className={`col-start-${item.col} row-start-${item.row}`}>
+                            <Link to={`/catalogue?content_type=${item.value}`}>{item.label}</Link>
+                        </div>
+                    ))}
+                    <div className="col-start-2 row-start-2">
+                    
+                    </div>
+                </div>
+                <Nav />
             </div>
-            <div>
-                <Link to='/catalogue?content_type=3'>diseño gráfico</Link>
-                <Link to='/catalogue?content_type=2'>diseño 3D</Link>
-            </div>
-            <div>
-                <Link to='/catalogue?content_type=1'>fotos</Link>
-            </div>
-            <Nav/>
         </>
     )
 }
