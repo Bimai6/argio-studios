e4f55bdb2556aca68a10232e4e5aa0e48c17277d
Bimai6 <bimaivfx@gmail.com>
2025-05-16 14:30:28 +0200

    refactor: change nav to manage an array to map the links

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,13 +1,27 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Nav = () => {
 
+    const location = useLocation();
+
+    const navItems = [
+        {link : '/about-us', content: 'sobre nosotros'},
+        {link : '/', content: 'principal'},
+        {link : '/contact', content: 'contacto'}
+    ]
+
     return (
         <>
-        <nav>
-        <Link to="/about-us">Sobre nosotros</Link>
-        <Link to="/">Principal</Link>
-        <Link to="/contact">Contacto</Link>
+        <nav className="fixed bottom-0 left-0 w-full h-[80px] z-50 px-4">
+            <ul className="flex flex-row justify-between gap-4">
+                {navItems.map((item, index) => (
+                    <li key={index}>
+                        <Link to={item.link} className={`py-2 px-4 rounded-4xl ${location.pathname === "/" ? "text-white bg-[#343434]" : "bg-[#D9D9D9]"}`}>
+                            {item.content}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </nav>
         </>
     )
