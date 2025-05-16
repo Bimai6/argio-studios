6ec399ae8f8191e047271b01348f3218f97a0373
Bimai6 <bimaivfx@gmail.com>
2025-05-16 19:03:04 +0200

    refactor: change to nav logic to show links depending on context

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,30 +1,41 @@
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation } from "react-router-dom";
 
 const Nav = () => {
-
     const location = useLocation();
+    const isHome = location.pathname === "/";
 
     const navItems = [
-        {link : '/about-us', content: 'sobre nosotros'},
-        {link : '/', content: 'principal'},
-        {link : '/contact', content: 'contacto'}
-    ]
+        { link: "/about-us", content: "sobre nosotros", justify: "justify-start pl-4" },
+        { link: "/", content: "principal", justify: "justify-center" },
+        { link: "/contact", content: "contacto", justify: "justify-end pr-4" }
+    ];
 
     return (
         <>
-        <nav className="fixed bottom-0 left-0 w-full h-[80px] z-50 px-4">
-            <ul className="flex flex-row justify-between gap-4">
-                {navItems.map((item, index) => (
-                    <li key={index}>
-                        <Link to={item.link} className={`py-2 px-4 rounded-4xl ${location.pathname === "/" ? "text-white bg-[#343434]" : "bg-[#D9D9D9]"}`}>
-                            {item.content}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-        </>
-    )
-}
+            <nav className="fixed bottom-0 left-0 w-screen h-[110px] z-50 px-4">
+                <ul className="grid grid-cols-3 w-full h-full text-center">
+                    {navItems.map((item, index) => {
+                        const defaultLink = (
+                            <Link
+                                to={item.link}
+                                className={`py-2 px-4 rounded-4xl ${
+                                    isHome ? "text-white bg-[#343434]" : "bg-[#D9D9D9]"
+                                }`}
+                            >
+                                {item.content}
+                            </Link>
+                        );
 
-export default Nav
+                        return (
+                            <li key={index} className={`flex items-center ${item.justify}`}>
+                                {item.link === "/" && isHome ? null : defaultLink}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+        </>
+    );
+};
+
+export default Nav;
