db9ae6a71c309ee454d6cbe3470b162d03b85634
Bimai6 <bimaivfx@gmail.com>
2025-05-16 18:58:52 +0200

    refactor: add utils to manage arrays used in multiple files

---
+++ frontend/src/utils/Arrays.jsx
@@ -0,0 +1,8 @@
+const filterItems = [
+    { label: "vídeos", value: "0", col: "2", row: "1"},
+    { label: "diseño gráfico", value: "3", col: "1", row: "2" },
+    { label: "diseño 3D", value: "2", col: "3", row: "2" },
+    { label: "fotos", value: "1", col: "2", row: "3" },
+];
+
+export default filterItems
