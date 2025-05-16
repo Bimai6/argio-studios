093274184bef0c1f21bcfacc0c420897ae3b7d6e
Bimai6 <bimaivfx@gmail.com>
2025-05-16 10:20:40 +0200

    feat: add components and basic structure to pages

--- frontend/src/pages/Catalogue/Catalogue.jsx
+++ frontend/src/pages/Catalogue/Catalogue.jsx
@@ -1,13 +1,21 @@
 import ListContent from "../../components/ListContent/ListContent"
+import Nav from "../../components/Nav/Nav"
 
 const Catalogue = () => {
 
+    const filterItems = ['vídeos', 'diseño gráfico', 'diseño 3D', 'fotos'];
+
     return(
         <>
-        <h1>This is Catalogue Page</h1>
+        <div>
+            {filterItems.map((item, index) => {
+                <p key={index}>{item}</p>
+            })}
+        </div>
         <div className="flex flex-col items-center w-full h-full">
             <ListContent />
         </div>
+        <Nav />
         </>
     )
 }
