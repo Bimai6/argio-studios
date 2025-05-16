9ffd032451c25d49a8f3ec5bd80d03a035e4ce28
Bimai6 <bimaivfx@gmail.com>
2025-05-16 18:58:52 +0200

    refactor: add utils to manage arrays used in multiple files

--- frontend/src/pages/Catalogue/Catalogue.jsx
+++ frontend/src/pages/Catalogue/Catalogue.jsx
@@ -1,45 +1,39 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import ListContent from "../../components/ListContent/ListContent";
 import Nav from "../../components/Nav/Nav";
-
-const filterItems = [
-  { label: "vídeos", value: "0" },
-  { label: "diseño gráfico", value: "3" },
-  { label: "diseño 3D", value: "2" },
-  { label: "fotos", value: "1" },
-];
+import filterItems from "../../utils/Arrays";
 
 const Catalogue = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const params = new URLSearchParams(location.search);
-  const contentType = params.get("content_type") || "0";
+    const location = useLocation();
+    const navigate = useNavigate();
+    const params = new URLSearchParams(location.search);
+    const contentType = params.get("content_type") || "0";
 
-  const handleChange = (value) => {
-    navigate(`/catalogue?content_type=${value}`);
-  };
+    const handleChange = (value) => {
+        navigate(`/catalogue?content_type=${value}`);
+    };
 
-  return (
+    return (
     <>
-      <div className="flex justify-between p-4">
-        {filterItems.map((item) => (
-          <button
-            key={item.value}
-            onClick={() => handleChange(item.value)}
-            className={`px-4 py-1  ${
-              contentType === item.value ? "rounded-4xl bg-[#343434] text-white" : ""
-            }`}
-          >
-            {item.label}
-          </button>
-        ))}
-      </div>
-      <div className="flex flex-col items-center w-full h-full">
-        <ListContent contentType={contentType} />
-      </div>
-      <Nav />
+        <div className="flex justify-between p-4">
+            {filterItems.map((item) => (
+                <button
+                key={item.value}
+                onClick={() => handleChange(item.value)}
+                className={`px-4 py-1  ${
+                contentType === item.value ? "rounded-4xl bg-[#343434] text-white" : ""
+                }`}
+                >
+                {item.label}
+                </button>
+            ))}
+        </div>
+        <div className="flex flex-col items-center w-full h-full">
+            <ListContent contentType={contentType} />
+        </div>
+        <Nav />
     </>
-  );
+    );
 };
 
 export default Catalogue;
