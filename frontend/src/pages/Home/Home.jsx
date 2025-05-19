b861e6868f56ef729257928f5393ad233f31654d
Bimai6 <bimaivfx@gmail.com>
2025-05-19 11:32:21 +0200

    feat: add background video to Home page.

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,7 +5,19 @@ import filterItems from "../../utils/Arrays"
 const Home = () => {
     return (
         <>
-            <div className="bg-black h-screen text-white relative">
+            <video
+            className="fixed top-0 left-0 w-full h-full object-cover -z-20"
+            autoPlay
+            muted
+            loop
+            playsInline
+            >
+                <source src="/videos/background_home.mp4" type="video/mp4" />
+            </video>
+
+            <div className="fixed top-0 left-0 w-full h-full bg-black opacity-80 -z-10"></div>
+
+            <div className="h-screen text-white relative z-50">
                 <div className="grid grid-cols-3 grid-rows-3 h-full place-items-center text-xl sm:text-2xl md:text-4xl xl:text-5xl">
                     {filterItems.map((item, index)=> (
                         <div key={index} className={`col-start-${item.col} row-start-${item.row}`}>
