2cd1173f427d0d84784064d471498008bd799c09
Bimai6 <bimaivfx@gmail.com>
2025-05-21 18:37:03 +0200

    feat: add ternary operator to ContentDetails to display diferents elements depending on the content_type

--- frontend/src/pages/ContentDetails/ContentDetails.jsx
+++ frontend/src/pages/ContentDetails/ContentDetails.jsx
@@ -11,6 +11,16 @@ const ContentDetails = () => {
 
     const content = location.state?.content;
 
+    const urlElement = content.content_type !== 0 ? (<img
+                    src={content.thumbnail_url}
+                    alt={content.alt}
+                    className="w-4/10 max-h-screen object-contain rounded-lg place-self-center"
+                />) : (<iframe 
+                    className="w-5/10 h-2/3 rounded-lg place-self-center"
+                    src={content.url}
+                    allowFullScreen>
+                    </iframe>);
+
     if (!content) {
         return <p>Contenido no disponible actualmente, disculpe las molestias</p>;
     }
@@ -18,11 +28,7 @@ const ContentDetails = () => {
     return (
         <>
             <div className="h-screen flex flex-row max-w-full overflow-hidden p-4 gap-4">
-                <img
-                    src={content.thumbnail_url}
-                    alt={content.alt}
-                    className="w-4/10 max-h-screen object-contain rounded-lg place-self-center"
-                />
+                {urlElement}
                 <div className="flex-1 flex flex-col overflow-hidden text-right text-[#343434]">
                     <button className="text-3xl self-end justify-self-start mb-10 rounded-full px-5 py-3 border-2 border-[#343434] hover:text-white hover:bg-[#343434]" onClick={handleBack}>&lt;</button>
                     <h1 className="text-6xl font-bold mb-9">{content.title}</h1>
