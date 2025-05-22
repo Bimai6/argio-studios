cc0a7523df4693e77ead7c5dc88e5b8da8b93bd1
Bimai6 <bimaivfx@gmail.com>
2025-05-22 13:53:50 +0200

    feat: add new global arrays on Arrays.jsx utility

--- frontend/src/utils/Arrays.jsx
+++ frontend/src/utils/Arrays.jsx
@@ -1,8 +1,18 @@
-const filterItems = [
+export const filterItems = [
     { label: "vídeos", value: "0", col: "2", row: "1"},
     { label: "diseño gráfico", value: "3", col: "1", row: "2" },
     { label: "diseño 3D", value: "2", col: "3", row: "2" },
     { label: "fotos", value: "1", col: "2", row: "3" },
 ];
 
-export default filterItems
+export const members = [
+    { name: "Alvaro Jesús Ramírez", role: "Diseñador Líder", img: "https://res.cloudinary.com/dr9vuz2td/image/upload/v1747901490/1747901322297_ohokoa.jpg", tasks: ["videografía", "fotografía", "diseño 3d", "diseño gráfico"]},
+    { name: "Alvaro Jesús Ramírez", role: "Diseñador Líder", img: "https://res.cloudinary.com/dr9vuz2td/image/upload/v1747901490/1747901322297_ohokoa.jpg", tasks: ["videografía", "fotografía", "diseño 3d", "diseño gráfico"]}
+];
+
+
+export const socialLinks = [
+    { name: "instagram", link: "#"},
+    { name: "linkedin", link: "#"},
+    { name: "pinterest", link: "#"}
+];
