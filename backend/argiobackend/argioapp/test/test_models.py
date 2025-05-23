a082eea507174a7aad5b97eda43edcbe64da1d4c
Bimai6 <bimaivfx@gmail.com>
2025-05-23 22:57:44 +0200

    test: add thumbnail_url check on test_content_create

--- backend/argiobackend/argioapp/test/test_models.py
+++ backend/argiobackend/argioapp/test/test_models.py
@@ -10,6 +10,7 @@ def test_content_create():
         " for an international brand with high reputation in the multimedia sector.",
         content_type= Content.ContentType.VIDEO, 
         url="https://res.cloudinary.com/dr9vuz2td/image/upload/v1746388461/untitled_nekgt3.png",
+        thumbnail_url="https://res.cloudinary.com/dr9vuz2td/image/upload/v1746388461/untitled_nekgt3.png"
     )
 
     assert content_instance.pk is not None
@@ -18,6 +19,7 @@ def test_content_create():
         " for an international brand with high reputation in the multimedia sector."
     assert content_instance.content_type == Content.ContentType.VIDEO
     assert content_instance.url == "https://res.cloudinary.com/dr9vuz2td/image/upload/v1746388461/untitled_nekgt3.png"
+    assert content_instance.thumbnail_url == "https://res.cloudinary.com/dr9vuz2td/image/upload/v1746388461/untitled_nekgt3.png"
     assert content_instance.is_active == True
     assert isinstance(content_instance.created_at, timezone.datetime)
     assert str(content_instance) == "Inspirational Video"
