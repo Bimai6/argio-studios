189ae2735dce5ec87447e131db5ba28f63d77703
Bimai6 <bimaivfx@gmail.com>
2025-05-31 19:01:54 +0200

    styles: minor changes on Contact styles

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -5,9 +5,9 @@ const Contact = () => {
 
     return(
         <>
-        <div data-testid="page-contact" className="flex">
+        <div data-testid="page-contact" className="flex flex-col xl:flex-row ">
             <ContactForm/>
-            <p className="ml-10 mt-8 mr-8 text-end">Aquí va una descripción que el equipo de ArgioStudios tiene que proveer al desarrollador...</p>
+            <p className="ml-10 xl:ml-60 mt-8 mr-8 text-end">Aquí va una descripción que el equipo de ArgioStudios tiene que proveer al desarrollador...</p>
         </div>
         <Nav />
         </>
