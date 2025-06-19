d0c7e16f96d226852c676dc900ae920805080b66
Bimai6 <bimaivfx@gmail.com>
2025-06-19 21:10:49 +0200

    styles: change mediaquery breakpoints on Contact page

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -6,12 +6,12 @@ const Contact = () => {
 
     return(
         <>
-        <div data-testid="page-contact" className="flex flex-col xl:flex-row justify-center">
-            <ContactForm className="order-2 xl:order-1"/>
-            <div className="flex flex-col mt-2 mb-10 order-1 xl:order-2">
+        <div data-testid="page-contact" className="flex flex-col lg:flex-row justify-center">
+            <ContactForm className="order-2 lg:order-1"/>
+            <div className="flex flex-col mt-2 mb-10 order-1 lg:order-2">
                 {contactParagraph.map((item, key) => {
                     return(
-                        <p key={key} className="text-xl sm:text-2xl md:text-3xl xl:text-4xl ml-10 xl:ml-30 mt-8 mr-15 text-center xl:text-start">{item.text}</p>
+                        <p key={key} className="text-xl sm:text-2xl md:text-3xl xl:text-4xl ml-10 xl:ml-30 mt-8 mr-15 text-center lg:text-start">{item.text}</p>
                     );
                 })}
             </div>
